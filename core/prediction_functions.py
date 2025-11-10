9bac58c97269efeca7e77f2b53ee85b39393d4bc
camiladasilvaa <camidsacosta@gmail.com>
2025-11-10 17:53:39 -0600

    Preprocessing functions

---
+++ core/prediction_functions.py
@@ -0,0 +1,117 @@
+import joblib
+from sentence_transformers import SentenceTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score, classification_report
+import pandas as pd
+import numpy as np
+
+# load classifiers
+EI_model = joblib.load("ei_model.joblib")
+SN_model = joblib.load("sn_model.joblib")
+TF_model = joblib.load("tf_model.joblib")
+JP_model = joblib.load("jp_model.joblib")
+
+# load embedding model
+embed_model = SentenceTransformer("embed_model_all_mpnet")
+
+def predict_EI(question_id, user_answer):
+    # prepare input as used during training
+    text = f"Q: {question_id} A: {user_answer}"
+
+    # get embedding
+    # [text] equivalent to using tolist()
+    embedding = embed_model.encode([text], convert_to_numpy=True)
+
+    # predict label
+    prob_E = EI_model.predict_proba(embedding)[0][1]
+
+    if prob_E >= 0.5:
+        pred_label = "E"
+    else:
+        pred_label = "I"
+
+    return pred_label, prob_E
+
+def predict_SN(question_id, user_answer):
+    text = f"Q: {question_id} A: {user_answer}"
+
+    embeddings = embed_model.encode([text], convert_to_numpy=True)
+
+    prob_S = SN_model.predict_proba(embeddings)[0][1]
+
+    if prob_S >= 0.5:
+        pred_label = "S"
+    else:
+        pred_label = "N"
+
+    return pred_label, prob_S
+
+def predict_TF(question_id, user_answer):
+    text = f"Q: {question_id} A: {user_answer}"
+
+    embeddings = embed_model.encode([text], convert_to_numpy=True)
+
+    prob_T = TF_model.predict_proba(embeddings)[0][1]
+
+    if prob_T >= 5:
+        pred_label = "T"
+    else:
+        pred_label = "F"
+
+    return pred_label, prob_T
+
+def predict_JP(question_id, user_answer):
+    text = f"Q: {question_id} A: {user_answer}"
+
+    embeddings = embed_model.encode([text], convert_to_numpy=True)
+
+    prob_J = JP_model.predict_proba(embeddings)[0][1]
+
+    if prob_J >= 5:
+        pred_label = "J"
+    else:
+        pred_label = "P"
+
+    return pred_label, prob_J
+
+def predict_dichotomy(question_id, user_answer, dichotomy):
+    text = f"Q: {question_id} A: {user_answer}"
+
+    embedding = embed_model.encode([text], convert_to_numpy=True)
+
+    if dichotomy == "E/I":
+        prob = EI_model.predict_proba(embedding)[0][1]
+
+        if prob >= 0.5:
+            pred_label = "E"
+        else:
+            pred_label = "I"
+    elif dichotomy == "S/N":
+        prob = SN_model.predict_proba(embedding)[0][1]
+
+        if prob >= 0.5:
+            pred_label = "S"
+        else:
+            pred_label = "N"
+    elif dichotomy == "T/F":
+        prob = TF_model.predict_proba(embedding)[0][1]
+
+        if prob >= 0.5:
+            pred_label = "T"
+        else:
+            pred_label = "F"
+    elif dichotomy == "J/P":
+        prob = JP_model.predict_proba(embedding)[0][1]
+
+        if prob >= 0.5:
+            pred_label = "J"
+        else:
+            pred_label = "P"
+
+    return pred_label, prob
+
+# user_input = "I usually keep to myself unless someone talks to me first."
+# label, probability = predict_EI(38, user_input)
+
+# print(label, probability)
---
+++ core/sn_preprocessing.py
@@ -0,0 +1,59 @@
+# Preprocesing architecture:
+# * Prepare training data
+# * Train a lightweight classifier
+# * Embed user response (with all-mpnet-base-v2 (SentenceTransformers))
+# * Pass embedding into your classifier to get E/I, S/N, ..., score
+# * Get probability score for each dichotomy ({ "E": 0.92, "I": 0.08 })
+# * Accumulate all answers and compute final MBTI (scoring)
+
+# install required libraries
+from sentence_transformers import SentenceTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score, classification_report
+import joblib
+import pandas as pd
+import numpy as np
+
+# embedding example/testing from documentation using SentenceTransformer
+# sentences = ["This is an example sentence", "Each sentence is converted"]
+
+# model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+# embeddings = model.encode(sentences)
+# print(embeddings)
+
+# read labeled data
+df = pd.read_json("data/sn_training.jsonl", lines=True)
+
+# embedding model
+embed_model = SentenceTransformer("embed_model_all_mpnet")
+
+# adapt data to include question and aswer for training -> better accuracy
+adapted_df = ("Q: " + df['question_id'].astype(str) + "A: " + df['text']).tolist()
+
+# compute embeddings
+embeddings = embed_model.encode(adapted_df, show_progress_bar=True, convert_to_numpy=True)
+
+# E/I training
+X = embeddings
+y = (df["label"] == 'S').astype(int).values # label-based indexer, 1 = S, 0 = N
+
+# 20% of data for testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+# create Logistic Regression model
+SN_model = LogisticRegression(max_iter=1000)
+
+# train model
+SN_model.fit(X_train, y_train)
+
+# evaluate model
+y_pred = SN_model.predict(X_test)
+y_prob =  SN_model.predict_proba(X_test)[:,1]
+
+print("Acc:", accuracy_score(y_test, y_pred))
+print("AUC:", roc_auc_score(y_test, y_prob))
+print(classification_report(y_test, y_pred))
+
+# save model
+joblib.dump(SN_model, "sn_model.joblib")
