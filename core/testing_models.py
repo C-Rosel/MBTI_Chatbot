807e31cdb570ac054237023b381faf45eaf15923
camiladasilvaa <camidsacosta@gmail.com>
2025-11-10 21:40:21 -0600

    Improved/simplified testing file

--- core/testing_models.py
+++ core/testing_models.py
@@ -1,11 +1,3 @@
-import joblib
-from sentence_transformers import SentenceTransformer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, roc_auc_score, classification_report
-import pandas as pd
-import numpy as np
-
 from prediction_functions import predict_JP
 
 user_input = "guide events through careful planning and choice"
