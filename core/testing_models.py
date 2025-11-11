a28d809e9d41d09f3d7530b2ed40bd580a3834c0
camiladasilvaa <camidsacosta@gmail.com>
2025-11-10 21:42:16 -0600

    Update testing_models.py

--- core/testing_models.py
+++ core/testing_models.py
@@ -1,4 +1,9 @@
-from prediction_functions import predict_JP
+from prediction_functions import predict_JP, predict_dichotomy
+
+user_input = "guide events through careful planning and choice"
+label, probability = predict_dichotomy(27, user_input, "J/P")
+
+print(label, probability)
 
 user_input = "guide events through careful planning and choice"
 label, probability = predict_JP(27, user_input)
