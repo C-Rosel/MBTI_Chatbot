a135abf594d49e301ccbf4f44a650622202412c5
camiladasilvaa <camidsacosta@gmail.com>
2025-11-11 07:53:32 -0600

    Results + Data viz

---
+++ results/tf_results.py
@@ -0,0 +1,55 @@
+import seaborn as sns
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+import joblib
+from sentence_transformers import SentenceTransformer
+
+# load classifier
+TF_model = joblib.load("models/tf_model.joblib")
+
+# S/N
+# read labeled data
+df = pd.read_json("data/tf_training.jsonl", lines=True)
+
+# embedding model
+embed_model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+
+# adapt data to include question and aswer for training -> better accuracy
+adapted_df = ("Q: " + df['question_id'].astype(str) + " A: " + df['text']).tolist()
+
+# compute embeddings
+embeddings = embed_model.encode(adapted_df, show_progress_bar=True, convert_to_numpy=True)
+
+X = embeddings
+y = (df["label"] == 'T').astype(int).values # label-based indexer, 1 = T, 0 = F
+
+# 20% of data for testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+# evaluate model
+y_pred = TF_model.predict(X_test)
+y_prob = TF_model.predict_proba(X_test)[:,1]
+
+# accuracy
+test_accuracy = accuracy_score(y_test, y_pred)
+print(f"Test accuracy: {test_accuracy:.4f}")
+
+# confusion matrix
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+# detailed per-class metrics
+print(classification_report(y_test, y_pred, target_names=['F', 'T']))
+
+# visualize results
+plt.figure(figsize=(6, 5))
+
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Oranges', xticklabels=['F', 'T'], yticklabels=['F', 'T'])
+
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.title('T/F Confusion Matrix')
+
+plt.show()
+
